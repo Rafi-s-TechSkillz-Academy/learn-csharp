81101f95c3c0385c39ea212ef1709d8248b5c6df
Rafi mohammad <88322835+rafi1827@users.noreply.github.com>
2023-07-13 10:40:03 +0530

    new code

---
+++ 20230713/basicscsharp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+int age; // declaring
+
+// Console.WriteLine(age); // error: use of unassigned local variable 'age'
+
+
+
+
+age = 18; // initializing
+
+Console.WriteLine(age); // error: use of unassigned local variable 'age'
+
+
+
+
+int rollNumber = 1, marks = 100; // declaring and initializing multiple variables
+
+Console.WriteLine($"{rollNumber} {marks}"); // string interpolation
+
+
+
+
+Console.WriteLine($"Int: {sizeof(int)} byte(s) of memory. Its Min and Max Values are {int.MinValue} and {int.MaxValue}");
+
+
+
+
+age = 25; // re-initializing
+
+Console.WriteLine(age);
+
+
+
+
+Console.WriteLine(int.Parse("123")); // converting string to int
