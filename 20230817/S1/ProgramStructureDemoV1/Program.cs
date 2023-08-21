80cee3a44c0214e65f9ca353b77975450d54f4d7
Rafi mohammad <88322835+rafi1827@users.noreply.github.com>
2023-08-21 10:51:55 +0530

    Rafi/12jun folderstructure (#7)
    * toplevel vs not top level
    
    added folder structure and added two programs one without top level which is demo1 and with top level  which is demo 2
    
    * new couple folders
    
    added couple folders one with existing folder and newfolder
    
    * completed 1
    
    * completed 1 again
    
    * comits on7/7
    
    * new code
    
    * new code
    
    * coding
    
    * new folder
    
    * new
    
    * 2 programs
    
    * last commit for now
    
    * Update Readme.md
    
    * 20230810-1
    
    * a1typecasting,a2vars
    
    * s1 completed
    
    * completed s2

---
+++ 20230817/S1/ProgramStructureDemoV1/Program.cs
@@ -0,0 +1,34 @@
+﻿using ProgramStructureDemoV1.Services;
+
+
+namespace ProgramStructureDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* Block of code Starts here */
+
+            WriteLine("Hello .NET 8 World!"); /* This is a statement and it ends with semi colon. */
+
+            WriteLine("Enter your name: ");
+            var name = ReadLine();
+            WriteLine($"Hello {name}!");
+
+            int num1 = 10, num2 = 20; /* Variable declaration and initialization. */
+            Maths maths = new(); /* Instantiation of Maths class. */
+            WriteLine($"{num1} + {num2} = {maths.AddNumbers(num1, num2)}");
+
+            /* Accessing static members of a class. */
+            WriteLine($"{num1} + {num2} = {Maths.AddNumbersV1(num1, num2)}");
+            WriteLine($"Max value is {Maths.MaxValue}");
+
+            /* The dot operator (.) is used to access members of a namespace, type, or object. */
+            WriteLine("Press any key to exit.");
+            ReadKey();
+
+            // Console.ReadKey(); & Console.Readkey(); are different /* C# is a case sensitive language. */
+
+        }
+    }
+}
---
+++ 20230817/S2/ProgramStructureDemoV3/Program.cs
@@ -0,0 +1,46 @@
+﻿/* 
+    Using VB.NET style namespace aliasing. We are using VB.NET Library in this C# program.
+*/
+using MathServicesVBLib.MathLibrary;
+using MathServicesCSLib;
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.WriteLine("Hello .NET 8 World!"); /* This is a statement and it ends with semi colon. */
+
+Console.Write("Enter your name: ");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello {name}!");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+
+int num1 = 10, num2 = 20; /* Variable declaration and initialization. */
+Maths maths = new(); /* Instantiation of Maths VB.NET Lib class. */
+Console.WriteLine($"Maths VB.NET Lib => {num1} + {num2} = {maths.AddNumbers(num1, num2)}");
+
+/* Accessing static members of a class. */
+Console.WriteLine($"Maths VB.NET Lib Static Method => {num1} + {num2} = {Maths.AddNumbersV1(num1, num2)}");
+Console.WriteLine($"Maths VB.NET Lib Static Member => Max value: {Maths.MaxValue}");
+
+Console.WriteLine($"Maths VB.NET Lib Static Member => Maths.MaxValue: {Maths.MaxValue.GetType()}.");
+int maxValue = Maths.MaxValue;
+Console.WriteLine($"(C# Variable) Max value: {maxValue} and its type {maxValue.GetType()}.");
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+MathsCS mathsCs = new(); /* Instantiation of Maths C# Lib class. */
+Console.WriteLine($"Maths C# Lib => {num1} + {num2} = {mathsCs.AddNumbers(num1, num2)}");
+
+/* Accessing static members of a class. */
+Console.WriteLine($"Maths C# Lib Static Method => {num1} + {num2} = {MathsCS.AddNumbersV1(num1, num2)}");
+Console.WriteLine($"Maths C# Lib Static Member => Max value: {MathsCS.MaxValue}");
+
+Console.WriteLine($"Maths C# Lib Static Member => Maths.MaxValue: {MathsCS.MaxValue.GetType()}.");
+maxValue = MathsCS.MaxValue;
+Console.WriteLine($"(C# Variable) Max value: {maxValue} and its type {maxValue.GetType()}.");
+
+Console.ResetColor();
+
+/* The dot operator (.) is used to access members of a namespace, type, or object. */
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
