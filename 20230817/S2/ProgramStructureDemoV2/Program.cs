13b327370b2f81d030cae356d25df360c58ca85e
Rafi mohammad <88322835+rafi1827@users.noreply.github.com>
2023-08-21 10:51:04 +0530

    completed s2

---
+++ 20230817/S2/ProgramStructureDemoV2/Program.cs
@@ -0,0 +1,27 @@
+﻿using ProgramStructureDemoV2.Services;
+
+
+/* Block of code Starts here */
+ForegroundColor = ConsoleColor.Green;
+WriteLine("Hello .NET 8 World!"); /* This is a statement and it ends with semi colon. */
+
+WriteLine("Enter your name: ");
+var name = ReadLine();
+WriteLine($"Hello {name}!");
+
+int num1 = 10, num2 = 20; /* Variable declaration and initialization. */
+Maths maths = new(); /* Instantiation of Maths class. */
+WriteLine($"{num1} + {num2} = {maths.AddNumbers(num1, num2)}");
+
+/* Accessing static members of a class. */
+WriteLine($"{num1} + {num2} = {Maths.AddNumbersV1(num1, num2)}");
+WriteLine($"Max value is {Maths.MaxValue}");
+
+ResetColor();
+
+/* The dot operator (.) is used to access members of a namespace, type, or object. */
+WriteLine("Press any key to exit.");
+ReadKey();
+
+// Console.ReadKey(); & Console.Readkey(); are different /* C# is a case sensitive language. */
+
